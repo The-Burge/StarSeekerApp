ec3f5f12039b6e7fdef95c7503f897feba6035bd
The-Burge <113515950+The-Burge@users.noreply.github.com>
2023-11-26 15:07:01 +0000

    Plug in API
    Plugging in of the API with the form submmissions

--- src/pages/journey.js
+++ src/pages/journey.js
@@ -1,27 +1,50 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, StyleSheet } from 'react-native';
+import { View, TextInput, Text, Button, StyleSheet } from 'react-native';
 
-const Journey = () => {
-  const [distance, setPassengerCount] = useState('');
-  const [passengerCount, setBagCount] = useState('');
+const JourneyCost = () => {
+  const [distance, setDistance] = useState('');
+  const [passengers, setPassengers] = useState('');
   const [parkingDays, setParkingDays] = useState('');
+  const [apiResponse, setApiResponse] = useState(null);
+
+  const handleApiRequest = async () => {
+    try {
+      const LocalEnv = require('../../.env.local.json')
+      const apiKey = LocalEnv.STARSEEKER_API_KEY;
+      const journeyUrl = `https://hstc-api.testing.keyholding.com/transport/${distance}?passengers=${passengers}&parking=${parkingDays}`;
+      const response = await fetch(journeyUrl, {
+        method: 'GET',
+        headers: {
+          'x-api-key': apiKey,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await response.json();
+      setApiResponse(data);
+    } catch (error) {
+      console.error('Error making API request:', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <Text style={styles.heading}>Journey Calculator</Text>
+
       <Text>Distance</Text>
       <TextInput
         style={styles.input}
         keyboardType="numeric"
         value={distance}
-        onChangeText={(text) => setPassengerCount(text)}
+        onChangeText={(text) => setDistance(text)}
       />
 
       <Text>Number of Passengers:</Text>
       <TextInput
         style={styles.input}
         keyboardType="numeric"
-        value={passengerCount}
-        onChangeText={(text) => setBagCount(text)}
+        value={passengers}
+        onChangeText={(text) => setPassengers(text)}
       />
 
       <Text>Number of Days of Parking:</Text>
@@ -31,6 +54,15 @@ const Journey = () => {
         value={parkingDays}
         onChangeText={(text) => setParkingDays(text)}
       />
+
+      <Button title="Submit" onPress={handleApiRequest} />
+
+      {apiResponse && (
+        <View style={styles.apiResponseContainer}>
+          <Text>API Response:</Text>
+          <Text>{JSON.stringify(apiResponse)}</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -42,6 +74,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
   },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
   input: {
     height: 40,
     borderColor: 'gray',
@@ -50,6 +87,12 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     width: 200,
   },
+  apiResponseContainer: {
+    marginTop: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
 });
 
-export default Journey;
+export default JourneyCost;
