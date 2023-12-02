2c21e2d1f7826aefce3fcfad787165ebf35ff53b
The-Burge <113515950+The-Burge@users.noreply.github.com>
2023-12-02 15:46:29 +0000

    Pull to refresh

--- src/pages/Gates.tsx
+++ src/pages/Gates.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  StatusBar,
+  RefreshControl,
+} from 'react-native';
 
 const Gates = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
   const LocalEnv = require('../../.env.local.json');
   const apiKey = LocalEnv.STARSEEKER_API_KEY;
 
-  useEffect(() => {
+  const fetchData = useCallback(() => {
     const apiUrl = 'https://hstc-api.testing.keyholding.com/gates';
 
     // Start loading
@@ -21,42 +30,55 @@ const Gates = () => {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => {
+      .then(response => {
         if (!response.ok) {
           throw new Error('Failed to fetch results');
         }
         return response.json();
       })
-      .then((responseJson) => {
+      .then(responseJson => {
         // Stop loading when data is received
         setLoading(false);
         setData(responseJson);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error('Error:', error);
         // Stop loading on error
         setLoading(false);
+      })
+      .finally(() => {
+        // Stop refreshing
+        setRefreshing(false);
       });
   }, [apiKey]);
 
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const handleRefresh = () => {
+    // Start refreshing
+    setRefreshing(true);
+    // Fetch data again
+    fetchData();
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {loading ? (
-        // Show loading indicator (rocket) while loading
-        <ActivityIndicator size="large" color="#3b5998" />
-      ) : (
-        // Show data when loaded
-        <FlatList
-          data={data.sort((a, b) => a.name.localeCompare(b.name))}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.itemText}>Gate Name: {item.name}</Text>
-              <Text style={styles.itemText}>Gate Code: {item.code}</Text>
-            </View>
-          )}
-        />
-      )}
+    <View style={styles.container}>
+      <FlatList
+        data={data.sort((a, b) => a.name.localeCompare(b.name))}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Gate Name: {item.name}</Text>
+            <Text style={styles.itemText}>Gate Code: {item.code}</Text>
+          </View>
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+        }
+      />
+      {loading && <ActivityIndicator size="large" color="#3b5998" />}
     </View>
   );
 };
@@ -64,7 +86,9 @@ const Gates = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
   },
   item: {
     padding: 10,
